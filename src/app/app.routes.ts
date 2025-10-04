daf29b64dfa66669279221821aeafa53d1e9085f
Jakub Rodzeń <jakubrodzen01@gmail.com>
2025-10-04 21:59:46 +0200

    create info page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { Home } from './pages/home/home';
 import { Events } from './pages/events/events';
 import { Map } from './pages/map/map';
+import { EventInfo } from './pages/event-info/event-info';
 
 export const routes: Routes = [
     {
@@ -10,7 +11,8 @@ export const routes: Routes = [
       { path: '', redirectTo: 'home', pathMatch: 'full' },
       { path: 'home', component: Home },
       { path: 'events', component: Events },
-      { path: 'map', component: Map}
+      { path: 'map', component: Map},
+      { path: 'event/:id', component: EventInfo}
     ],
   },
   { path: '**', redirectTo: '' },
---
+++ src/app/pages/event-info/event-info.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-event-info',
+  imports: [],
+  templateUrl: './event-info.html',
+  styleUrl: './event-info.scss'
+})
+export class EventInfo {
+
+}
