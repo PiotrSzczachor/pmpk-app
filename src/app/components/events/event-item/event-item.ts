323091ec2667231654f2719e4b579f233bcdcc6c
Jakub Rodzeń <jakubrodzen01@gmail.com>
2025-10-04 20:26:23 +0200

    Done event items

--- src/app/components/events/event-item/event-item.ts
+++ src/app/components/events/event-item/event-item.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-event-item',
-  imports: [],
+  imports: [MatIconModule],
   templateUrl: './event-item.html',
   styleUrl: './event-item.scss'
 })
