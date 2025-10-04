a29d827617fc5f62ac3a4fa516952abb5e792bd8
Jakub Rodzeń <jakubrodzen01@gmail.com>
2025-10-04 20:00:46 +0200

    done filters view

--- src/app/components/events/events-filters/events-filters.ts
+++ src/app/components/events/events-filters/events-filters.ts
@@ -1,15 +1,12 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { MatDatepickerModule } from '@angular/material/datepicker';
-import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-events-filters',
-  imports: [FormsModule, MatIconModule, MatFormFieldModule, MatDatepickerModule],
+  imports: [FormsModule, MatIconModule],
   templateUrl: './events-filters.html',
   styleUrl: './events-filters.scss'
 })
 export class EventsFilters {
-  selectedDate: Date | null = null;
 }
