d89a48e29b468a1f0198510738119061ac70a991
Jakub Rodzeń <jakubrodzen01@gmail.com>
2025-10-05 02:25:51 +0200

    map

--- src/app/pages/map/map.ts
+++ src/app/pages/map/map.ts
@@ -1,7 +1,9 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { EventsFilters } from '../../components/events/events-filters/events-filters';
 import { EventsMap } from '../../components/events/events-map/events-map';
+import { AppStore } from '../../store/app.store';
+import { EventsService } from '../../api';
 
 @Component({
   selector: 'app-map',
@@ -9,6 +11,19 @@ import { EventsMap } from '../../components/events/events-map/events-map';
   templateUrl: './map.html',
   styleUrl: './map.scss'
 })
-export class Map {
-  
+export class Map implements OnInit {
+  appStore = inject(AppStore);
+
+  constructor(private eventsService: EventsService) {
+
+  }
+
+  ngOnInit(): void {
+    this.eventsService.eventsGet().subscribe({
+        next: (e) => this.appStore.setEvents(e),
+            error: (err) => {
+                console.error('Events failed', err);
+            }
+        });
+  }
 }
